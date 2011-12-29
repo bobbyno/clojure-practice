aeaeb1631b55268cbec62e7e209642a678ad26b2
Ecology Pair <ecology@drw.com>
2011-12-29 17:32:11 -0600

    refactoring to more time optimal solution

--- test/euler/test/one.clj
+++ test/euler/test/one.clj
@@ -6,5 +6,17 @@
 
 (deftest problem
   (testing "sum of natural numbers below x that are multiples of 3 or 5"
-    (is (= 23 (calc 10)))
-    (is (= 233168 (calc 1000)))))
+    (is (= 23 (time (calc 10))))
+    (is (= 233168 (time (calc 1000))))
+    (is (= 233333166668 (time (calc 1e6))))
+    (is (= 23333331666668 (time (calc 1e7))))
+	; one billion will take almost 2 mins in the current impl!
+	; (is (= 233333333166666668 (time (calc 1e9))))
+))
+
+; target=999
+; Function SumDivisibleBy(n)
+;  p=target div n
+;  return n*(p*(p+1)) div 2
+; EndFunction
+; Output SumDivisibleBy(3)+SumDivisibleBy(5)-SumDivisibleBy(15) 
