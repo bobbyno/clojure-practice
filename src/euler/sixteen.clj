1513e98b28930b1309a4b4a28e5459addca997d6
Bobby Norton <bobby@testedminds.com>
2012-01-02 17:38:41 -0600

    Solved problem sixteen

---
+++ src/euler/sixteen.clj
@@ -0,0 +1,9 @@
+(ns euler.sixteen)
+
+(defn two-str 
+  "Represent the value of raising 2 to the given power as a string with no loss of precision"
+  [n]
+  (.toPlainString (BigDecimal. (Math/pow 2 n))))
+
+(defn sum-digits [n]
+  (reduce + (map #(Integer/parseInt (str %)) (seq (two-str n)))))
---
+++ test/euler/test/sixteen.clj
@@ -0,0 +1,13 @@
+(ns euler.test.sixteen
+  (:use [euler.sixteen]
+        [clojure.test]))
+
+(deftest test-two-str
+  (is (= "1024" (two-str 10)))
+  (is (= "1267650600228229401496703205376" (two-str 100)))
+)
+
+(deftest test-sum-digits
+  (is (= 26 (sum-digits 15)))
+  (is (= 1366 (sum-digits 1000)))
+)
