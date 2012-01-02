a9eb1f78dc28651c3bdc5431ada97135e4489f13
Bobby Norton <bobby@testedminds.com>
2012-01-02 14:50:56 -0600

    refactored to solution in Programming Clojure

--- src/euler/two.clj
+++ src/euler/two.clj
@@ -1,30 +1,17 @@
 (ns euler.two)
 
-(defn calculate-fib 
-  [first second k n]
-  (if (= k n) 
-    second
-	(recur second (+ first second) (inc k) n)))
+; from Programming Clojure, p. 157
+(defn fibonacci-seq []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
 
-(defn fibonacci
-  [n]
-  (if (or (= n 0) (= n 1))
-	n
-    (calculate-fib 0 1 1 n)))
-
-(defn fibonacci-seq 
-  []
-  (let [infinity (iterate #(inc %) 0)]
-	(map fibonacci infinity)))
-
-(defn fib-values-under
-  [max]  
+(defn fib-values-under [max]  
   (take-while #(< % max) (fibonacci-seq)))
 
-(defn k-fib-terms
-  [k]
+(defn k-fib-terms [k]
   (take k (fibonacci-seq)))
 
-(defn sum-even-valued-terms-under
-  [n]
+(defn sum-even-valued-terms-under [n]
   (reduce + (filter even? (fib-values-under n))))
+
+(defn fibonacci [k]
+  (last (take k (fibonacci-seq))))
--- test/euler/test/two.clj
+++ test/euler/test/two.clj
@@ -4,14 +4,14 @@
 		
 ; http://projecteuler.net/problem=2		
 
-(deftest test-fibonacci
-  (is (= 0 (fibonacci 0)))
-  (is (= 1 (fibonacci 1)))
+(deftest test-fibonacci-terms
+  (is (= 0 (fibonacci 1)))
   (is (= 1 (fibonacci 2)))
-  (is (= 2 (fibonacci 3)))
-  (is (= 3 (fibonacci 4)))
-  (is (= 5 (fibonacci 5)))
-  (is (= 6765 (fibonacci 20)))
+  (is (= 1 (fibonacci 3)))
+  (is (= 2 (fibonacci 4)))
+  (is (= 3 (fibonacci 5)))
+  (is (= 5 (fibonacci 6)))
+  (is (= 4181 (fibonacci 20)))
 )
 
 (deftest test-fib-under
