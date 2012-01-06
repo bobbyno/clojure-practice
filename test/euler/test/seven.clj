f17fbfaa7f2252f4eb115c2f374fc57aa76e33d0
Bobby Norton <codeculturist@gmail.com>
2012-01-06 13:48:15 -0600

    Added lazy sequence solution for seven

---
+++ test/euler/test/seven.clj
@@ -0,0 +1,19 @@
+(ns euler.test.seven
+  (:use [clojure.test]
+		[euler.seven]))
+		
+
+(deftest test-is-prime
+  (is (true?  (is-prime 2)))
+  (is (true?  (is-prime 3)))
+  (is (true?  (is-prime 5)))
+  (is (true?  (is-prime 7)))
+  (is (true?  (is-prime 31)))
+  (is (false? (is-prime 30)))
+)
+
+(deftest test-nth-prime
+  (is (= 13  	(nth-prime 6)))
+  (is (= 541 	(nth-prime 100)))
+  (is (= 224737 (nth-prime 20000)))
+)
--- test/euler/test/three.clj
+++ test/euler/test/three.clj
@@ -2,11 +2,8 @@
   (:use [clojure.test]
 	    [euler.three]))
 
-(deftest test-factors
-  (is (= #{1} (factors 1)))
-  (is (= #{1 2} (factors 2)))
-  (is (= #{1 2 4} (factors 4)))
-  (is (= #{1 2 5 10} (factors 10)))
-  (is (= #{1 2 3 6 9 18} (factors 18)))
-  (is (= #{1 2 3 4 6 9 12 18 36} (factors 36)))
-)
+
+;(deftest test-prime-factors
+;  (is (= '(2 5) (prime-factors 10)))
+;  (is (= '(5 7 13 29) (prime-factors 13195)))
+;)
